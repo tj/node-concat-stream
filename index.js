2cfffdccfec25c492fcfd713789d88204ccedcee
TJ Holowaychuk <tj@vision-media.ca>
2013-12-18 09:05:16 -0800

    refactor

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 
 var Readable = require('stream').Readable;
 
-module.exports = function concat(stream, fn) {
-  
-  if (!fn)
-    throw new Error('callback should be a function');
-  
-  if (!stream || !(stream instanceof Readable))
-    return fn(new Error('stream should be an instance of stream.Readable'));
-  
+module.exports = function(stream, fn) {
+  if (!fn) throw new Error('callback required');
+
   var buf = [];
-  var fnCalled = false;
+  var done = false;
 
   stream.on('data', function(c){
     buf.push(c);
   });
 
   stream.on('end', function(){
-    if (!fnCalled) {
-      fnCalled = true;
-      fn(null, Buffer.concat(buf));
-    }
+    if (done) return;
+    done = true;
+    fn(null, Buffer.concat(buf));
   });
-  
+
   stream.on('error', function(e){
-    if (!fnCalled) {
-      fnCalled = true;
-      fn(e);
-    }
+    if (done) return;
+    done = true;
+    fn(e);
   });
-  
 }
